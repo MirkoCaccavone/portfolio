5337cbe8d589bb3ae42bb8c2b9fd2152c658810d
Mirko <mirkocaccavone03@gmail.com>
2025-09-05 14:20:51 +0200

    Add routes in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,27 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Toaster } from 'react-hot-toast';
+import HomePage from './pages/HomePage';
+import NlsProject from './components/Projects/NlsProject';
+import Rentora from './components/Projects/Rentora';
+import BoolRoad from './components/Projects/BoolRoad';
+
 function App() {
 
   return (
     <>
+      <Toaster />
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<HomePage />} />
+          <Route path="/projects/next-level-shop" element={<NlsProject />} />
+          <Route path="/projects/rentora" element={<Rentora />} />
+          <Route path='/projects/boolroad' element={<BoolRoad />} />
+        </Routes>
+
+      </BrowserRouter>
 
     </>
   )
 }
 
-export default App
+export default App;
